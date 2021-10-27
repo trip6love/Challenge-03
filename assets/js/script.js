742c3878c7dd97fdd96645f6a4eca07077a41ca6
trip6love <lanebyers13@gmail.com>
2021-10-27 15:51:49 -0400

    Creating the shell

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+//How long would you like your password to be? (min 8 - max 128)
+function passwordLength () {
+  var userPasswordLength = window.prompt('Password length? Min 8 characters - max 128');
+  console.log(userPasswordLength);
+}
+
+//Would you like uppercase?
+function upperCase () {
+  var userUpperCase = window.confirm('Would you like upper case? Click OK for yes and CANCEL for no.');
+  console.log(userUpperCase);
+}
+
+//Would you like lowercase?
+function lowercase () {
+  var userLowerCase = window.confirm('Would you like lower case? Click OK for yes and CANCEL for no.');
+  console.log(userLowerCase);
+}
+
+//Would you like to include numbers?
+function numberTrue () {
+  var userNumberTrue = window.confirm('Would you like to include numbers? Click OK for yes and CANCEL for no.');
+  console.log(userNumberTrue);
+}
+
+//Would you like to include special Characters?
+function specialCharacters () {
+  var userSpecialCharacters = window.confirm('Would you like to use special characters? Click OK for yes and CANCEL for no.');
+  console.log (userSpecialCharacters);
+}
+
+//validate each input, make sure at least one character type is selected
+
+//generate password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
